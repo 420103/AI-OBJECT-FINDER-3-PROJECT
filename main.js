8163c453b38e613bbeff7cdee8fa389acb395f11
Tejash.bhola <90960004+420103@users.noreply.github.com>
2021-09-25 16:22:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+video = "";
+status1 = "";
+objects = [];
+object_input = document.getElementById("object_input");
+
+function preload(){
+}
+
+function setup(){
+   canvas = createCanvas(480, 380);
+   canvas.center();
+   video = createCapture(VIDEO);
+   video.hide()
+}
+
+function start(){
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    status1 = true;
+}
+
+function draw(){
+    image(video, 0, 0, 480, 380);
+    if(status1 !=""){
+        objectDetector.detect(video, gotResult);    
+        for(i = 0; i < objects.length; i++){
+            document.getElementById("status").innerHTML = "Status : Object Detected!";
+            document.getElementById("number_of_objects").innerHTML = "Number of objects detected are : "+ objects.length;
+
+            fill("red");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("red");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+
+            if(objects[i].label == object_input){
+                SpeechSynthesisUtterance.speak(object_input + "Detected!");
+            }
+        }
+    }
+
+}
+
+function gotResult(error, results){
+        if(error){
+            console.log(error); 
+        }
+        console.log(results);
+        objects = results;
+    }
